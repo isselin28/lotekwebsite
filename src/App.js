1ccd8e25a928c74a7d5c31c0fcdb8cb7bccbdce2
isselin28 <isselin.moektijono@gojek.com>
2021-10-31 21:55:48 +0800

    create delivery section and footer

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ import foodLotekPrepare from "./assets/food_lotek_prepare.png";
 import foodKolakBubur from "./assets/food_kolakbubur.jpg";
 import foodKolakCampur from "./assets/food_kolakcampur.jpg";
 import foodKariayam from "./assets/food_kariayam.jpg";
+import houseInside from "./assets/house_inside.png";
+import gofood from "./assets/gofood.png";
+import grabfood from "./assets/grabfood.png";
+import shopeefood from "./assets/shopeefood.png";
 
 const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
 `;
 
 const ContentWrapper = styled.div`
@@ -18,6 +23,15 @@ const ContentWrapper = styled.div`
   align-items: center;
   justify-content: center;
   gap: 40px;
+  margin-bottom: 30px;
+`;
+
+const DeliveryWrapper = styled(ContentWrapper)`
+  gap: 30px;
+  border: 2px solid #94703a;
+  border-radius: 10px;
+  padding: 10px 50px;
+  margin-bottom: 30px;
 `;
 
 const Image = styled.img`
@@ -30,8 +44,9 @@ const Image = styled.img`
 `;
 
 const StripImage = styled.img`
-  max-height: 80vh;
-  width: 100vw;
+  max-height: 660px;
+  height: 80vh;
+  width: 100%;
   object-fit: cover;
 `;
 
@@ -39,7 +54,7 @@ const Column = styled.div`
   display: flex;
   flex-wrap: wrap;
   flex-direction: column;
-  width: 50%;
+  flex: 1;
   gap: 40px;
 
   ${({ top }) =>
@@ -64,6 +79,7 @@ const TextLabel = styled.div`
   color: #605e5e;
   letter-spacing: 2px;
   line-height: 2;
+  text-transform: uppercase;
 
   margin-bottom: 4px;
 `;
@@ -88,11 +104,49 @@ const TextDescription = styled.div`
   margin-bottom: 4px;
 `;
 
+const FooterContainer = styled.div`
+  max-height: 240px;
+  height: 200px;
+  width: 100%;
+  object-fit: cover;
+  background-color: #94703a;
+  color: white;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+const IconWrapper = styled(TextLabel)`
+  display: flex;
+  color: white;
+  font-size: 13px;
+  padding-bottom: 4px;
+  gap: 8px;
+`;
+
+const SocialMediaWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  left: 220px;
+  top: 40px;
+`;
+
+const Copywright = styled(TextDescription)`
+  color: white;
+  font-size: 12px;
+  letter-spacing: 1px;
+  position: absolute;
+  bottom: 10px;
+`;
+
 function App() {
   return (
     <>
       <NavBar />
-      <StripImage src={houseImage} alt="House" />
+      <StripImage src={houseImage} alt="Lotek Kalipah Apo" />
       <Container>
         <ContentWrapper>
           <Column top="150">
@@ -144,6 +198,24 @@ function App() {
             />
           </Column>
         </ContentWrapper>
+        <DeliveryWrapper>
+          <TextTitle>Deliver to Your Doorstep</TextTitle>
+          <Image width="140" height="140" src={gofood} alt="Kari Ayam" />
+          <Image width="140" height="140" src={grabfood} alt="Kari Ayam" />
+          <Image width="180" height="180" src={shopeefood} alt="Kari Ayam" />
+        </DeliveryWrapper>
+        <StripImage src={houseInside} alt="Lotek Kalipah Apo" />
+        <FooterContainer>
+          <SocialMediaWrapper>
+            <IconWrapper>
+              <i class="fab fa-instagram fa-2x" /> lotekkalipahapo42
+            </IconWrapper>
+            <IconWrapper>
+              <i class="fab fa-whatsapp fa-2x" /> 812-123-123-123
+            </IconWrapper>
+          </SocialMediaWrapper>
+          <Copywright>&copy; 2021 by Isselin</Copywright>
+        </FooterContainer>
       </Container>
     </>
   );
