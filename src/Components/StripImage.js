bfc4db047fa10d3f78d845ece377dc0a6d899fe4
isselin28 <isselin.moektijono@gojek.com>
2021-12-04 02:11:14 +0800

    create strip image component

---
+++ src/Components/StripImage.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const Parallax = styled.div`
+  background-image: url(${(props) => props.imageUrl});
+  min-height: 600px;
+
+  background-attachment: fixed;
+  background-position: ${(props) => props.position};
+  background-repeat: no-repeat;
+  background-size: ${(props) => props.size};
+
+  transform: translateZ(2px) scale(0.9);
+
+  ${(props) => props.rest}
+`;
+
+function StripImage(props) {
+  const { imageUrl, position, size, ...rest } = props;
+  return (
+    <Parallax imageUrl={imageUrl} position={position} size={size} rest={rest} />
+  );
+}
+
+export default StripImage;
