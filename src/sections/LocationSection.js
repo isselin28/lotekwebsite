5b53a7985c2983ecae2c5671ce0f9e1ef46b4864
isselin28 <isselin.moektijono@gojek.com>
2021-12-22 22:06:10 +0800

    refactor location section

--- src/sections/LocationSection.js
+++ src/sections/LocationSection.js
@@ -9,31 +9,21 @@ import { Container } from "../components/Container";
 import { Image } from "../components/Image";
 
 const SectionContainer = styled(Container)`
+  width: 80vw;
   margin-top: 20vh;
   margin-bottom: 5vh;
 `;
 
-const LocationBox = styled(Container).attrs({
-  align: "center",
-  justify: "center",
-})`
-  width: 80%;
-  border: 1px solid ${({ theme }) => theme.primary.dark};
-  border-radius: 5px;
-  margin-bottom: 60px;
-`;
-
 export function LocationSection() {
   return (
-    <SectionContainer align="center" justify="center" direction="column">
-      <div id="find_us" />
-      <Container
-        direction="column"
-        justify="center"
+    <Container align="center" justify="center">
+      <SectionContainer
         align="center"
-        gap="10"
-        margin="40"
+        justify="center"
+        direction="column"
+        gap="15"
       >
+        <div id="find_us" />
         <Typography variant="title">Find Us</Typography>
         <Typography variant="body">
           *Due to Covid measurement, we only offers takeaway and online
@@ -44,10 +34,8 @@ export function LocationSection() {
           <Image width="115" height="110" src={gofood} alt="gojek" />
           <Image width="100" height="100" src={grabfood} alt="grab" />
         </Container>
-      </Container>
-      <LocationBox>
         <LocationBar />
-      </LocationBox>
-    </SectionContainer>
+      </SectionContainer>
+    </Container>
   );
 }
